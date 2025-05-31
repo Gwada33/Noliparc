1f4f95c6f2831399aac5319fcefdcc9e66ed53a8
Gwada33 <gwadagwada22@gmail.com>
2025-05-31 18:28:59 -0400

    Change Metadata

--- src/app/anniversaires/layout.tsx
+++ src/app/anniversaires/layout.tsx
@@ -3,11 +3,12 @@ import { ReactNode } from 'react';
 export const metadata = {
   title: 'Anniversaires - Fêtez chez Noliparc',
   description: 'Organisez une fête d’anniversaire inoubliable pour vos enfants à Noliparc, avec animations, salle dédiée et plus !',
-}
-
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
+    <>
       {children}
+    </>
   );
 }
